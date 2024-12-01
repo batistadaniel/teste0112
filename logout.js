e554d077887f59b957b71d911647a58459ca1fbb
Daniel BATISTA <109699716+batistadaniel@users.noreply.github.com>
2024-12-01 19:47:05 -0300

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,7 @@
+import { auth } from "./firebase-config.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+document.getElementById("logout-button").addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "index.html";
+});
