f5e9e951ad8443b02ae55a26d90cdf8f69d5f293
Daniel BATISTA <109699716+batistadaniel@users.noreply.github.com>
2024-12-01 20:20:14 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAtgfGJyv1IR0ZfOR5TLkkCXphmQ21sUTQ",
   authDomain: "teste-firebase-2-ab055.firebaseapp.com",
   databaseURL: "https://teste-firebase-2-ab055-default-rtdb.firebaseio.com",
   projectId: "teste-firebase-2-ab055",
-  storageBucket: "teste-firebase-2-ab055.appspot.com",
+  storageBucket: "teste-firebase-2-ab055.firebasestorage.app",
   messagingSenderId: "104641738429",
   appId: "1:104641738429:web:61805f610a8636d7e069d4",
-  measurementId: "G-Y0GKD7L3ZC",
+  measurementId: "G-Y0GKD7L3ZC"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { auth, database };
